021586afcc2c0d7371549f5f0fb3390f7648c3be
Arnaldo Liro <dinholiro@gmail.com>
2025-08-26 12:08:11 -0300

    feat: creating basic controller crud

--- src/main/java/br/com/apirest/apirest/SampleRestController.java
+++ src/main/java/br/com/apirest/apirest/SampleRestController.java
@@ -1,5 +1,30 @@
 package br.com.apirest.apirest;
 
-public class SampleRestController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+
+
+@RestController
+@RequestMapping("/api")
+public class SampleRestController {
+    
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("echo")
+    public String echoMessage(@RequestBody String message) {
+        
+        return "You sent: " + message;
+    }
+
+    
+    
+    
 }
