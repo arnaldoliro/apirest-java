e086734d58fa18d06761acb77a52efc81ab897e3
Arnaldo Liro <dinholiro@gmail.com>
2025-08-26 12:13:09 -0300

    fix: removing unnecessary imports

--- src/main/java/br/com/apirest/apirest/SampleRestController.java
+++ src/main/java/br/com/apirest/apirest/SampleRestController.java
@@ -3,7 +3,6 @@ package br.com.apirest.apirest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,7 +23,7 @@ public class SampleRestController {
         return "You sent: " + message;
     }
 
-    
+
     
     
 }
